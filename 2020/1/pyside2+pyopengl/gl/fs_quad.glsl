ccc5078098dca45149437ad6ceab5e8e2bdad483
minu jeong <minu.hanwool@gmail.com>
2020-01-06 17:32:23 +0000

    update kata/pyopengl, glfw, mesh info

--- 2020/1/pyside2+pyopengl/gl/fs_quad.glsl
+++
@@ -1,9 +0,0 @@
-#version 460
-
-in vec4 vs_pos;
-out vec4 fs_color;
-
-void main()
-{
-    fs_color = vec4(1.0, 0.0, 0.0, 1.0);
-}
--- 2020/1/pyside2+pyopengl/main.py
+++ 2020/1/pyside2+pyopengl/main.py
@@ -56,6 +56,20 @@ class Mesh(object):
         # compile shaders
         self.compile(vs_src, fs_src)
 
+    def uniform(self, uname, uvalue):
+        try:
+            uloc = gl.glGetUniformLocation(self.program, uname)
+
+            if isinstance(uvalue, (m.mat4)):
+                gl.glUniformMatrix4fv(uloc, 1, gl.GL_FALSE, m.value_ptr(uvalue))
+            elif isinstance(uvalue, (float)):
+                gl.glUniform1f(uloc, 1, gl.GL_FALSE, m.value_ptr(uvalue))
+            # ... add more types to use
+
+        except Exception as e:
+            log.info("[Mesh] Error in uniform")
+            log.debug(e)
+
     def compile(self, vs_src, fs_src):
         vs = gl.glCreateShader(gl.GL_VERTEX_SHADER)
         gl.glShaderSource(vs, vs_src)
@@ -78,85 +92,38 @@ class Mesh(object):
         T = glfw.get_time()
 
         if self.program is not None:
-            try:
-                gl.glUseProgram(self.program)
+            gl.glUseProgram(self.program)
 
-                u_MVP_loc = gl.glGetUniformLocation(self.program, "u_MVP")
-                M = m.translate(m.mat4(1.0), m.vec3(0.0, -0.7, 0.0))
-                campos = m.vec3(math.cos(T) * 2.0, 0.0, math.sin(T) * 2.0)
-                V = m.lookAt(campos, m.vec3(0.0), m.vec3(0.0, 1.0, 0.0))
-                P = m.perspective(m.radians(74.0), 1.0, 0.001, 100.0)
-                u_MVP = P * V * M
-                gl.glUniformMatrix4fv(u_MVP_loc, 1, gl.GL_FALSE, m.value_ptr(u_MVP))
+            M = m.translate(m.mat4(1.0), m.vec3(0.0, -0.7, 0.0))
+            campos = m.vec3(math.cos(T) * 2.0, 0.0, math.sin(T) * 2.0)
+            V = m.lookAt(campos, m.vec3(0.0), m.vec3(0.0, 1.0, 0.0))
+            P = m.perspective(m.radians(74.0), 1.0, 0.001, 100.0)
 
-            except Exception as e:
-                log.info("[Render] Error in program")
-                log.debug(e)
+            u_MVP = P * V * M
+            self.uniform("u_MVP", u_MVP)
 
         if self.vao is not None:
-            try:
-                gl.glBindVertexArray(self.vao)
-                gl.glDrawElements(
-                    gl.GL_TRIANGLES, self.len_indices, gl.GL_UNSIGNED_SHORT, None
-                )
-
-            except Exception as e:
-                log.info("[Render] Error in draw")
-                log.debug(e)
-
-
-# NOT USED
-class Quad(Mesh):
-    def __init__(self, vs_src, fs_src):
-        super(Quad, self).__init__(vs_src, fs_src)
-
-        # build vertex arrays..
-        self.vao = gl.glGenVertexArrays(1)
-        gl.glBindVertexArray(self.vao)
-
-        # build vertex buffer..
-        vertices = [-1, -1, +1, -1, -1, +1, +1, +1]
-        vbo, ibo = gl.glGenBuffers(2)
-
-        in_pos = gl.glGetAttribLocation(self.program, "in_pos")
-        gl.glBindBuffer(gl.GL_ARRAY_BUFFER, vbo)
-        gl.glBufferData(
-            gl.GL_ARRAY_BUFFER,
-            (c.c_float * len(vertices))(*vertices),
-            gl.GL_STATIC_DRAW,
-        )
-        gl.glVertexAttribPointer(in_pos, 2, gl.GL_FLOAT, gl.GL_FALSE, 0, None)
-        gl.glEnableVertexAttribArray(in_pos)
-        gl.glBindBuffer(gl.GL_ARRAY_BUFFER, 0)
-
-        # build index buffer..
-        indices = [0, 1, 2, 1, 2, 3]
-        self.len_indices = len(indices)
-        gl.glBindBuffer(gl.GL_ELEMENT_ARRAY_BUFFER, ibo)
-        gl.glBufferData(
-            gl.GL_ELEMENT_ARRAY_BUFFER,
-            (c.c_ushort * self.len_indices)(*indices),
-            gl.GL_STATIC_DRAW,
-        )
+            gl.glBindVertexArray(self.vao)
+            gl.glDrawElements(
+                gl.GL_TRIANGLES, self.len_indices, gl.GL_UNSIGNED_SHORT, None
+            )
 
 
 class AlbericBordeleaux(Mesh):
     def __init__(self):
+
+        dirname = "D:/Local/Python/gl_kata/2020/1/pyside2+pyopengl"
         super(AlbericBordeleaux, self).__init__(
-            readshader("D:/Local/Python/gl_kata/2020/1/pyside2+pyopengl/gl/vs_ab.glsl"),
-            readshader("D:/Local/Python/gl_kata/2020/1/pyside2+pyopengl/gl/fs_ab.glsl"),
+            readshader("{}/gl/vs_ab.glsl".format(dirname)),
+            readshader("{}/gl/fs_ab.glsl".format(dirname)),
         )
 
         # build vertex arrays..
         self.vao = gl.glGenVertexArrays(1)
         gl.glBindVertexArray(self.vao)
 
-        vbpath = (
-            "D:/Local/Python/~2019/~12/nsight_mesh_extract/mesh/alberic_bordeleaux.vbo"
-        )
-        ibpath = (
-            "D:/Local/Python/~2019/~12/nsight_mesh_extract/mesh/alberic_bordeleaux.ibo"
-        )
+        vbpath = ("{}/mesh/alberic_bordeleaux.vbo".format(dirname))
+        ibpath = ("{}/mesh/alberic_bordeleaux.ibo".format(dirname))
         vertices, indices = readvb(vbpath), readib(ibpath)
         vbo, ibo = gl.glGenBuffers(2)
 
@@ -181,46 +148,46 @@ class AlbericBordeleaux(Mesh):
         )
 
 
-class Client(object):
-    @staticmethod
-    def on_key(window, key, scan, act, mods):
+class Client(Thread):
+    def on_key(self, window, key, scan, act, mods):
         # hit space to close window
         if key == glfw.KEY_SPACE:
             log.info("key spacebar hit to closing..")
-            glfw.set_window_should_close(Client.window, glfw.TRUE)
+            glfw.set_window_should_close(self.window, glfw.TRUE)
 
-    @staticmethod
-    def renderloop(queue=None):
-        Client.queue = queue or Queue()
+    def __init__(self, queue=None):
+        super(Client, self).__init__()
+        self.queue = queue or Queue()
+
+    def start(self):
 
         # init window/gl
         glfw.init()
-        glfw.window_hint(glfw.FLOATING, glfw.TRUE)
-        Client.window = glfw.create_window(1024, 1024, "", None, None)
-        glfw.make_context_current(Client.window)
+        self.window = glfw.create_window(1024, 1024, "Render Window Test", None, None)
+        glfw.make_context_current(self.window)
 
         # init client
-        Client.init()
+        self.init()
 
-        glfw.set_key_callback(Client.window, Client.on_key)
+        glfw.set_key_callback(self.window, self.on_key)
 
         # render loop
-        starttime = time.time()
-        while (
-            not glfw.window_should_close(Client.window)
-            or starttime + 10.0 < time.time()
-        ):
-            glfw.poll_events()
-            glfw.swap_buffers(Client.window)
-            Client.render()
+        try:
+            while not glfw.window_should_close(self.window):
+                glfw.poll_events()
+                glfw.swap_buffers(self.window)
+                self.render()
 
-    @staticmethod
-    def init():
+        except Exception as e:
+            log.debug(e)
+
+        glfw.terminate()
+
+    def init(self):
         # check device OpenGL version
-        Client.albericborderleaux = AlbericBordeleaux()
+        self.albericborderleaux = AlbericBordeleaux()
 
-    @staticmethod
-    def render():
+    def render(self):
         # clear frame buffer..
         gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
 
@@ -231,13 +198,13 @@ class Client(object):
         gl.glDepthMask(gl.GL_TRUE)
         gl.glDepthFunc(gl.GL_LESS)
 
-        Client.albericborderleaux.render()
+        self.albericborderleaux.render()
 
 
 def main():
     queue = Queue()
-    t1 = Thread(target=Client.renderloop, args=(queue,))
-    t1.start()
+    client_thread = Client(queue)
+    client_thread.start()
 
 
 if __name__ == "__main__":
