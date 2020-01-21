4b3e86d39386999761ce94ce91e4b2d2384a11fe
minu jeong <minu.hanwool@gmail.com>
2020-01-21 17:13:45 +0000

    update kata: add simple glfw, update simple standalone

--- 2020/1/vdasvdsafd/main.py
+++
@@ -1,8 +0,0 @@
-import moderngl as mg
-import numpy as np
-import imageio as ii
-gl = mg.create_standalone_context()
-colortex = gl.texture((1024, 1024), 4, dtype="f4")
-gl.framebuffer([colortex]).use()
-gl.vertex_array(gl.program(vertex_shader=open("./gl/vs.glsl").read(), fragment_shader=open("./gl/fs.glsl").read()), [(gl.buffer(np.array([-1, -1, -1, 1, 1, -1, 1, 1], dtype=np.float32)), "2f", "in_pos", )], gl.buffer(np.array([0, 1, 2, 2, 1, 3], dtype=np.int32))).render()
-ii.imwrite("output.png", np.multiply(np.frombuffer(colortex.read(), dtype=np.float32), 255.0).astype(np.uint8).reshape((1024, 1024, 4))[::-1])
---
+++ 2020/1/vdasvdsafd/main_simple_glfw.py
@@ -0,0 +1,4 @@
+import glfw; import moderngl as mg; import numpy as np
+glfw.init(); win = glfw.create_window(1024, 1024, "glfw", None, None); glfw.make_context_current(win); gl = mg.create_context()
+vao = gl.vertex_array(gl.program(vertex_shader=open("./gl/vs.glsl").read(), fragment_shader=open("./gl/fs.glsl").read()), [(gl.buffer(np.array([-1, -1, -1, 1, 1, -1, 1, 1], dtype=np.float32)), "2f", "in_pos", )], gl.buffer(np.array([0, 1, 2, 2, 1, 3], dtype=np.int32)))
+while not glfw.window_should_close(win): glfw.poll_events(); glfw.swap_buffers(win); vao.render()
