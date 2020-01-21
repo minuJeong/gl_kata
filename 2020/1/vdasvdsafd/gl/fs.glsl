13d5372068071508e6f758c831f1cab588eebba1
minu jeong <minu.hanwool@gmail.com>
2020-01-21 17:42:33 +0000

    update kata

--- 2020/1/vdasvdsafd/gl/fs.glsl
+++ 2020/1/vdasvdsafd/gl/fs.glsl
@@ -6,6 +6,8 @@
 in vec2 vs_pos;
 out vec4 fs_color;
 
+uniform float u_time;
+
 struct March
 {
     float travel;
@@ -45,11 +47,11 @@ float sdf_world(inout March march)
 bool raymarch(vec3 o, vec3 r, inout March march)
 {
     float d;
-    for (int i = 0; i < 128; i++)
+    for (int i = 0; i < 24; i++)
     {
         march.pos = o + r * march.travel;
         d = sdf_world(march);
-        if (d < NEAR || march.travel > FAR) { return march.travel < FAR; }
+        if (d < NEAR || march.travel > FAR) { break; }
         march.travel += d;
     }
     return march.travel < FAR;
@@ -60,17 +62,13 @@ vec3 normalat(vec3 pos)
     const vec2 e = vec2(NEAR, 0.0);
 
     March march;
-    march.pos = pos;
-
     March marchx;
-    marchx.pos = pos - e.xyy;
-
     March marchy;
-    marchy.pos = pos - e.yxy;
-
     March marchz;
+    march.pos = pos;
+    marchx.pos = pos - e.xyy;
+    marchy.pos = pos - e.yxy;
     marchz.pos = pos - e.yyx;
-    
     return normalize(sdf_world(march) - vec3(
         sdf_world(marchx),
         sdf_world(marchy),
@@ -82,12 +80,14 @@ void main()
 {
     vec2 uv = vs_pos.xy;
 
-    vec3 pos = vec3(-2.0, 2.0, -5.0);
+    vec3 pos = vec3(-5.0, 2.0, -5.0);
+    pos.x = cos(u_time) * 5.0;
+    pos.z = sin(u_time) * 5.0;
     vec3 ray = lookat(pos, ZERO) * normalize(vec3(uv, 1.0));
 
     March march;
     vec3 RGB = vec3(0.2 - uv.y * 0.2);
-    if (raymarch(pos, ray, march))
+    if (raymarch(pos, ray, march)) {}
     {
         vec3 P = march.pos;
         vec3 N = normalat(P);
@@ -106,6 +106,5 @@ void main()
 
         RGB = vec3(light);
     }
-
     fs_color = vec4(RGB, 1.0);
 }
---
+++ 2020/1/vdasvdsafd/main_with_watchdog.py
@@ -0,0 +1,42 @@
+import glfw
+import moderngl as mg
+import numpy as np
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
+
+
+def init(vao):
+    try:
+        return gl.vertex_array(
+            gl.program(
+                vertex_shader=open("./gl/vs.glsl").read(),
+                fragment_shader=open("./gl/fs.glsl").read(),
+            ), [(gl.buffer(np.array([-1, -1, -1, 1, 1, -1, 1, 1], dtype=np.float32)), "2f", "in_pos")],
+            gl.buffer(np.array([0, 1, 2, 2, 1, 3], dtype=np.int32)),
+        )
+    except Exception as e:
+        print(e)
+        return vao
+
+glfw.init()
+glfw.window_hint(glfw.FLOATING, glfw.TRUE)
+win = glfw.create_window(1024, 1024, "glfw", None, None)
+glfw.make_context_current(win)
+gl = mg.create_context()
+vao = init(None)
+recompile = False
+def set_recompile(e):
+    global recompile
+    recompile = True
+h = FileSystemEventHandler()
+h.on_modified = set_recompile
+o = Observer()
+o.schedule(h, "./gl/", True)
+o.start()
+while not glfw.window_should_close(win):
+    glfw.poll_events()
+    glfw.swap_buffers(win)
+    vao.program["u_time"].value = glfw.get_time()
+    vao.render()
+    if recompile:
+        vao = init(vao)
